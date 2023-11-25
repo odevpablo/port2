1755101e3ba2e474bb19cf9f9cd45bfa989a232d
SrPabaum <mrmster6@gmail.com>
2023-11-25 16:37:10 -0300

    update

--- src/components/slideInfo.js
+++ src/components/slideInfo.js
@@ -5,6 +5,8 @@ import "slick-carousel/slick/slick-theme.css";
 import iDisk from '../imgs/IDisk.png'
 import Node from '../imgs/NODE JS.png'
 import Cep from '../imgs/searchcep.png'
+import Model1 from '../imgs/Landing page.png'
+
 import { DiReact } from "react-icons/di";
 import { FaNodeJs, FaGitAlt } from "react-icons/fa";
  
@@ -23,7 +25,7 @@ const Carousel = () => {
         <div className="slide-item">
           <h3>Plataforma iDisk</h3>
           <button className="slide-button">
-          <img src={iDisk} width={285}/>
+         <a href="https://idisk.netlify.app/"><img src={iDisk} width={285}/></a>
           <div className="elements">
             <p>A iDisk é minha plataforma onde os usuários podem acessar cursos
             voltados para a área de desenvolvimento web.</p>
@@ -34,20 +36,30 @@ const Carousel = () => {
         <div className="slide-item">
           <h3>CRUD Node.js</h3>
           <button className="slide-button">
-          <img src={Node} width={285}/>
+         <a href="https://github.com/odevpablo/reviewnodejs"><img src={Node} width={285}/></a>
           <div className="elements">
             <p>Desenvolvi esta CRUD em Node.js com o intuito de revisar conceitos básicos do Node em uma API.</p>
-            <a href=""><FaGitAlt size={25}/></a> 
+            <a href="https://github.com/odevpablo/reviewnodejs"><FaGitAlt size={25}/></a> 
           </div>
           </button>
         </div>
         <div className="slide-item">
           <h3>Rastreador de CEP</h3>
           <button className="slide-button">
-          <img src={Cep} width={285}/>
+          <a href="https://github.com/odevpablo/searchcep"><img src={Cep} width={285}/></a>
           <div className="elements">
             <p>Projeto desenvolvido em Angular no qual foi criada uma API que retorna o endereço do CEP procurado.</p>
-            <a href=""><FaGitAlt size={25}/></a> 
+            <a href="https://github.com/odevpablo/searchcep"><FaGitAlt size={25}/></a> 
+          </div>
+          </button>
+        </div>
+        <div className="slide-item">
+          <h3>Modelo landing page</h3>
+          <button className="slide-button">
+          <a href="https://mytemplateodevpablo.netlify.app/"><img src={Model1} width={285}/></a>
+          <div className="elements">
+            <p>Modelo de landing page escritório de advocacia</p>
+            <a href="https://github.com/odevpablo/template1"><FaGitAlt size={25}/></a> 
           </div>
           </button>
         </div>
